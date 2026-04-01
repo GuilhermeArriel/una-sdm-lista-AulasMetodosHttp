ffe1d23dd87f0e8ef5e13daefc91d775748df07d
Guilherme Arriel <guiarriel@gmail.com>
2026-03-31 23:10:52 -0300

    Add files via upload

---
+++ AulaMetodosHttp/Models/Produto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaMetodosHttp.Models
+{
+    public record Produto(
+        int Id,
+        string Nome,
+        decimal Preco
+    );
+    
+        
+    
+}
